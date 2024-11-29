355281e17bb28f3d435ce18152376f6a8a7dc76c
João Vaccari <joaopedrov_vaccari@hotmail.com>
2024-11-29 15:19:18 -0300

    Adicionei Comentario idiota no projeto

--- Assets/Scripts/EnemyPaddleController.cs
+++ Assets/Scripts/EnemyPaddleController.cs
@@ -11,6 +11,8 @@ public class EnemyPaddleController : MonoBehaviour
 
     private GameObject ball;
 
+
+    // Código mt foda meu
     // Start is called before the first frame update
     void Start()
     {
